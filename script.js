1b216dde00b09bb700cefa2510be0d484baa5881
trollballsgame <lanceephraimlanada@gmail.com>
2025-07-05 21:04:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const rarities = [
+    { name: "classical", chance: 1 / 499999, color: "#ffffff" },
+    { name: "error",     chance: 1 / 404201, color: "#ff0033" },
+    { name: "mythic",    chance: 1 / 75000,  color: "#ff00cc" },
+    { name: "Ethereal",  chance: 1 / 60300,  color: "#a100ff" },
+    { name: "Legendary", chance: 1 / 5000,   color: "#ff9900" },
+    { name: "lucky",     chance: 1 / 777,    color: "#00ffff" },
+    { name: "Rare",      chance: 1 / 100,    color: "#007bff" },
+    { name: "fake",      chance: 1 / 50,    color: "#007n7cf" },
+    { name: "Uncommon",  chance: 1 / 10,     color: "#22aa22" },
+    { name: "electric",  chance: 1 / 5,      color: "#ffff00" },
+    { name: "Common",    chance: 1 / 2,      color: "#888888" }
+]
+  
+function roll() {
+    const resultEl = document.getElementById("result")
+    const totalChance = rarities.reduce((sum, r) => sum + r.chance, 0);
+    let rand = Math.random() * totalChance;
+  
+    for (let r of rarities) {
+        rand -= r.chance;
+        const oneIn = Math.round(1 / r.chance).toLocaleString();
+        if (rand <= 0) {
+            if (r.name === "classical") {
+                startCutscene("ok now this is rare");
+                return;
+            }
+            const msg = `You got a <span style="color:${r.color}; font-weight:bold;">${r.name.toUpperCase()}</span> with a chance of 1 in ${oneIn}`;
+            resultEl.innerHTML = msg;
+            return;
+            }
+        }
+    }
+  
