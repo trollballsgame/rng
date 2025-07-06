74b115e8d270293b77dfec90e5b6dcf2b54b8537
trollballsgame <lanceephraimlanada@gmail.com>
2025-07-06 15:07:07 +0800

    Update script.js
    fixed

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+let iscutscene = false
+
+const rarities = [
+  { name: "[redacted]",      chance: 1 / 1500000, color: "#00ff00" },  
+  { name: "KHDGWBNVIK====",  chance: 1 / 1450000, color: "#ffffff" },
+  { name: "transcendant",    chance: 1 / 1000000, color: "#0ff0ff" },
+  { name: "classical",       chance: 1 / 499999,  color: "#ffffff" },
+  { name: "error",           chance: 1 / 404201,  color: "#ff0033" },
+  { name: "glow - extreme",  chance: 1 / 99999,   color: "#ffd4f0" },
+  { name: "mythic",          chance: 1 / 75000,   color: "#ff00cc" },
+  { name: "Ethereal",        chance: 1 / 60300,   color: "#a100ff" },
+  { name: "Legendary",       chance: 1 / 5000,    color: "#ff9900" },
+  { name: "lucky",           chance: 1 / 777,     color: "#00ffff" },
+  { name: "Rare",            chance: 1 / 100,     color: "#007bff" },
+  { name: "fake",            chance: 1 / 50,      color: "#0077cf" },
+  { name: "glow",            chance: 1 / 15,      color: "#ffeeaa" },
+  { name: "Uncommon",        chance: 1 / 10,      color: "#22aa22" },
+  { name: "morning",         chance: 1 / 7,       color: "#ffd75e" },
+  { name: "electric",        chance: 1 / 5,       color: "#ffff00" },
+  { name: "usual",           chance: 1 / 4,       color: "#96eb7a" },
+  { name: "Common",          chance: 1 / 2,       color: "#888888" }
+];
+
+function startCutscene(text) {
+  iscutscene = true
+  const cutscene = document.getElementById("cutscene");
+  const cutsceneText = document.getElementById("cutscene-text");
+  cutsceneText.textContent = text;
+  cutscene.classList.remove("hidden");
+
+  setTimeout(() => {
+    cutscene.classList.add("hidden");
+    iscutscene = false
+  }, 3000);
+}
+
 function roll() {
   if (iscutscene) return;
   const resultEl = document.getElementById("result");
